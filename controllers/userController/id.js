048c2fc7d60a7e68f6a630491180d704e864b7c6
dev-Shivam999 <shivamswami982@gmail.com>
2024-04-06 23:45:46 +0530

    addfriend

--- controllers/userController/id.js
+++ controllers/userController/id.js
@@ -1,18 +1,35 @@
-import { Message } from "../../models/models.js";
+import { Message, friend } from "../../models/models.js";
+import { tokenone } from "../../utils/utils.js";
 
 export const id= async (req, res) => {
   const { id } = req.body;
-
+  const {token} =await req.cookies;
   try {
     const be = await Message.findById({ _id: id });
+    
+    const blName = await Message.findOne({ _id: token });
+  
+    const existingFriend = await friend.findOne({ userId: id });
+   
+
+      const lol=existingFriend.for.some(i=>i.email==blName.email)
 
     if (!be) {
       return res.json({ error: true, message: "user not found" });
-    } else {
+    } else if(lol) {
       return res.json({
         error: false,
         user:be,
+        add:true
       });
+    }
+    else{
+      return res.json({
+        error: false,
+        user:be,
+        add:false
+      });
+
     }
   } catch (error) {
     return res.json({ error: true, message: "user not found" });
