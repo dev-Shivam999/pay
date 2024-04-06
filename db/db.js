f4435b54df7672c85847513f4b477f98cdabd516
Shivam Swami <123632262+dev-Shivam999@users.noreply.github.com>
2024-04-06 22:47:05 +0530

    Update db.js

--- db/db.js
+++ db/db.js
@@ -5,7 +5,7 @@ import dotenv, { config } from "dotenv"
 config({ path: "./.env" })
 export const db= async()=>{
     try {
-        await mongoose.connect("mongodb+srv://shivamswami982:5ov3GxbPMMWtXtBQ@cluster0.xgwd95i.mongodb.net/pay")
+        await mongoose.connect("mongodb+srv://shivamswami982:5ov3GxbPMMWtXtBQ@cluster0.xgwd95i.mongodb.net/pay",{ useNewUrlParser: true, useUnifiedTopology: true, serverSelectionTimeoutMS: 30000 })
     .then( () => console.log("Connected to MongoDB"))
 
     .catch((err) => console.error("Error connecting to MongoDB:", err));
